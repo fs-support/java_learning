ec2de27a6902c9312ee500719bafd5ef07b114eb
fs-support <technical_education_support@freestyles.jp>
2019-10-29 17:14:29 +0900

    20191008_156739212：実力診断問題：入門編　2.ifの理解

---
+++ コードレビュー/01_入門/20191008_156739212/Exam0204_class.java
@@ -0,0 +1,32 @@
+﻿package Exam0204;
+
+import java.util.Scanner;	//読み込むやつ
+
+public class Exam0204_class {
+
+	public static void main(String[] args) {
+
+		// 入力した値を読み込む
+		Scanner stdin = new Scanner(System.in);
+		int num1 = 0;
+		num1 = stdin.nextInt();
+
+		//正か不か判断
+		if(num1 > 0) {
+			System.out.println("input number : " +num1);
+			System.out.println();
+			System.out.println(num1+ " is Positive.");
+		}else if (num1 < 0){
+			System.out.println("input number : " +num1);
+			System.out.println();
+			System.out.println(num1 + " is Negative.");
+		}else{
+			System.out.println("input number : " + num1);
+
+
+		}
+
+
+	}
+
+}
