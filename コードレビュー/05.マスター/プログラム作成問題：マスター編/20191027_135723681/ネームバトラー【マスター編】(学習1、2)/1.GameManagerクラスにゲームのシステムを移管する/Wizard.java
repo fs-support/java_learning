c5b20e3dfb9232094b3f62765fedf099c786dbf4
fs-support <technical_education_support@freestyles.jp>
2019-11-06 15:03:27 +0900

    20191027_135723681：プログラム作成問題：マスター編　ネームバトラー(学習1)

---
+++ コードレビュー/05.マスター/プログラム作成問題：マスター編/20191027_135723681/ネームバトラー【マスター編】(学習1、2)/1.GameManagerクラスにゲームのシステムを移管する/Wizard.java
@@ -0,0 +1,88 @@
+package name_battlerM1;
+
+import java.util.Random;
+
+public class Wizard extends Player{
+
+	Random rnd = new Random();
+
+	// =======================
+	// フィールド変数
+	// =======================
+
+	// =======================
+	// コンストラクタ
+	// =======================
+	public Wizard(String name)
+	{
+		super(name);
+	}
+	// =======================
+	// Getter / Setter
+	// =======================
+
+	// =======================
+	// protected メソッド
+	// =======================
+	/**
+	 * 名前(name)からキャラクターに必要なパラメータを生成する
+	 */
+	@Override
+	protected void MakeCharacter()
+	{
+		// 魔法使いのパラメータを名前から生成する
+		this.hp = GetNumber(0, 100) + 50;
+		this.mp = GetNumber(0, 50) + 30;;
+		this.str = GetNumber(1, 49) + 1;
+		this.def = GetNumber(2, 49) + 1;
+		this.luck = GetNumber(3, 99) + 1;
+		this.agi = GetNumber(4, 40) + 20;
+	}
+	/**
+	 * 対象プレイヤーに攻撃を行う
+	 * @param defender : 対象プレイヤー
+	 */
+	@Override
+	public void Attack(Player defender)
+	{
+
+		int damage = 0;
+		//MPが残っている場合は魔法をランダムで使用して攻撃
+		if(GetMP() >= 10) {
+
+			int select = rnd.nextInt(2);
+
+			if(select == 0 && (GetMP() >= 20)  ) {
+
+				damage = Thunder();
+			}
+			else {
+
+				damage = Fire();
+			}
+		}
+		else {
+		//MPが0の時に通常攻撃をする
+        // 与えるダメージを求める
+        System.out.println(GetName() + "の攻撃！");
+        damage = CalcDamage(defender);
+		}
+
+        // 求めたダメージを対象プレイヤーに与える
+        System.out.println(defender.GetName() + "に" + damage + "のダメージ！");
+        defender.Damage(damage);
+
+        // 倒れた判定
+        if (defender.GetHP() <= 0) {
+            System.out.println(defender.GetName() + "は力尽きた...");
+        }
+	}
+	// =======================
+	// private メソッド
+	// =======================
+
+
+	// =======================
+	// public メソッド
+	// =======================
+}
