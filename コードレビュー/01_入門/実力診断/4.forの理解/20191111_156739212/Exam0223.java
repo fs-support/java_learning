fdf24b823c044bb6e1b181c228f65d363e431577
fs-support <technical_education_support@freestyles.jp>
2019-11-12 11:34:31 +0900

    20191111_156739212：実力診断問題：入門編　4.forの理解

---
+++ コードレビュー/01_入門/実力診断/4.forの理解/20191111_156739212/Exam0223.java
@@ -0,0 +1,18 @@
+package Exam0223;
+
+public class Exam0223 {
+
+	public static void main(String[] args) {
+
+		for(int data=0; data<=10; data+=2) {
+			System.out.print(data + " ");
+		}
+
+	}
+
+}
+
+
+//問題　Exam0223
+//forを使用して、0～10の値を1つおきに出力するプログラムを作成してください。
+//0 2 4 6 8 10
