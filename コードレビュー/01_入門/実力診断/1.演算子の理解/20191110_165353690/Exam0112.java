4a5ba8cd8ab9552cf80e0cb2afd3158591b29d47
fs-support <technical_education_support@freestyles.jp>
2019-11-12 11:07:07 +0900

    20191110_165353690：実力診断問題：入門編　1.演算子の理解

---
+++ コードレビュー/01_入門/実力診断/1.演算子の理解/20191110_165353690/Exam0112.java
@@ -0,0 +1,13 @@
+class Exam0112{
+
+public static void main(String[] args){
+int data1 = 13;
+int data2 = 4;
+
+SystemOut.printf(data1+data2);
+SystemOut.printf(data1-data2);
+SystemOut.printf(data1*data2);
+SystemOut.printf(data1/data2);
+SystemOut.printf(data1%data2);
+	}
+}
