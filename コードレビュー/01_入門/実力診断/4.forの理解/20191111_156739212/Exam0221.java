7a7f49ab6e337efdf10d0a078cadf5ae9f04e222
fs-support <technical_education_support@freestyles.jp>
2019-11-12 11:32:28 +0900

    20191111_156739212：実力診断問題：入門編　4.forの理解

---
+++ コードレビュー/01_入門/実力診断/4.forの理解/20191111_156739212/Exam0221.java
@@ -0,0 +1,17 @@
+﻿package Exam0221;
+
+public class Exam0221 {
+
+	public static void main(String[] args) {
+
+		for(int data =1; data<=10; data++) {
+			System.out.print(data +" ");
+		}
+	}
+}
+
+
+//問題　Exam0221
+//forを使用して、1～10の値を出力するプログラムを作成してください。
+//1 2 3 4 5 6 7 8 9　　10までを出力するとあるのに答えは9まで・・？？
+
